5f7409e3e7733a1bf45560701ee17a06f2ed9ac4
Yejia Liu <yejia@Yejias-MacBook-Pro.local>
2020-03-18 18:26:22 -0700

    run all tests

--- .github/runTests.sh
+++ .github/runTests.sh
@@ -6,14 +6,12 @@ do
         extension="${filename##*.}"
         parentdir="$(basename "$(dirname "$file")")"
         echo parent_dir:${parentdir}
-        # name="${filename%.*}"
-        # test_file_name=./dataprep/tests/${parentdir}/${name}_test.py
-        # echo ${test_file_name}
-        # poetry run pytest ${test_file_name}
-        
+        if [[ "$parentdir" =~ ^(dblp|spotify|twitter|yelp)$ ]]; then
+            for testfile in ./${parentdir}/tests/*:
+            do
+                echo testfile:"$testfile"
+                poetry run pytest $testfile
+            done
+        fi  
 done
-# poetry run pytest ./dataprep/tests/eda/test.py
-# poetry run pytest ./dataprep/tests/eda/test_plot.py
-# poetry run pytest ./dataprep/tests/eda/test_plot_correlation.py
-# poetry run pytest ./dataprep/tests/eda/test_plot_missing.py
 
