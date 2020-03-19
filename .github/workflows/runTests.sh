557e6d37ee3a4864326bf528c57e753a8bb735b2
Yejia Liu <yejia@Yejias-MacBook-Pro.local>
2020-03-18 18:27:49 -0700

    run all tests

--- .github/workflows/runTests.sh
+++ .github/workflows/runTests.sh
@@ -13,14 +13,4 @@ do
                 poetry run pytest $testfile
             done
         fi  
-        # name="${filename%.*}"
-        # test_file_name=./dataprep/tests/${parentdir}/${name}_test.py
-        # echo ${test_file_name}
-        # poetry run pytest ${test_file_name}
-        
 done
-# poetry run pytest ./dataprep/tests/eda/test.py
-# poetry run pytest ./dataprep/tests/eda/test_plot.py
-# poetry run pytest ./dataprep/tests/eda/test_plot_correlation.py
-# poetry run pytest ./dataprep/tests/eda/test_plot_missing.py
-
